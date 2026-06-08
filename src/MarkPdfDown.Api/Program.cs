65ac124fbfe8a0e8a3d20f88ef350bcf8471ee2b
Marco Minerva <marco.minerva@gmail.com>
2026-06-08 11:06:26 +0200

    Add project files.

---
+++ src/MarkPdfDown.Api/Program.cs
@@ -0,0 +1,36 @@
+using TinyHelpers.AspNetCore.Extensions;
+using TinyHelpers.AspNetCore.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddOpenApi(options =>
+{
+    options.RemoveServerList();
+    options.AddDefaultProblemDetailsResponse();
+
+});
+
+builder.Services.AddDefaultProblemDetails();
+builder.Services.AddDefaultExceptionHandler();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+app.UseExceptionHandler();
+app.UseStatusCodePages();
+
+app.MapOpenApi();
+app.MapSwaggerUI(setupAction: options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", builder.Environment.ApplicationName);
+});
+
+app.UseRouting();
+
+app.Run();
